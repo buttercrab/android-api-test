476b1eee353623702dad8328e67c5c5cdc4fb497
Hyunbin Lee <46880076+jshyunbin@users.noreply.github.com>
2019-11-05 16:13:41 +0900

    Added RecyclerView layout

--- app/src/main/java/com/example/android_api_test/MainActivity.java
+++ app/src/main/java/com/example/android_api_test/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android_api_test;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.location.Location;
@@ -14,6 +15,7 @@ import android.os.Message;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.IOException;
 import java.util.Calendar;
@@ -21,11 +23,14 @@ import java.util.Date;
 
 public class MainActivity extends Activity {
 
-    LocationManager locationManager;
-
-    TextView time, date, temp, minmaxtemp;
-
     private class MyLocationListener implements LocationListener {
+
+        Context context;
+
+        MyLocationListener(Context context) {
+            super();
+            this.context = context;
+        }
 
         @Override
         public void onLocationChanged(Location location) {
@@ -44,9 +49,25 @@ public class MainActivity extends Activity {
 
         @Override
         public void onProviderDisabled(String provider) {
-
+            new AlertDialog.Builder(this.context)
+                    .setTitle("GPS Lost Connection")
+                    .setMessage("Please Turn on your GPS to get weather information of your location.")
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                            System.exit(0);
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
         }
     }
+
+    LocationManager locationManager;
+
+    TextView time, date, temp, minmaxtemp;
+    RecyclerView forecast;
 
     Handler updateTime = new Handler(Looper.getMainLooper()) {
         @SuppressLint({"DefaultLocale", "SetTextI18n"})
@@ -54,7 +75,8 @@ public class MainActivity extends Activity {
         public void handleMessage(Message msg) {
             Calendar calendar = Calendar.getInstance();
             time.setText(calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE));
-            date.setText(String.format("%d/%d/%d", calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)));
+            date.setText(String.format("%d/%d/%d", calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)));
             sendEmptyMessageDelayed(1, 100);
         }
     };
@@ -81,13 +103,18 @@ public class MainActivity extends Activity {
                     .setIcon(android.R.drawable.ic_dialog_alert)
                     .show();
         }
+
         time = findViewById(R.id.current_time);
         date = findViewById(R.id.current_date);
         temp = findViewById(R.id.temp);
         minmaxtemp = findViewById(R.id.minmaxtemp);
+        forecast = findViewById(R.id.weather_forecast);
+
         updateTime.sendEmptyMessage(1);
-        LocationListener locationListener = new MyLocationListener();
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+
+        LocationListener locationListener = new MyLocationListener(this);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10,
+                locationListener);
     }
 
     protected void loadWeatherData(Location loc) {
@@ -95,9 +122,10 @@ public class MainActivity extends Activity {
             Weather weather = GetWeather.getWeather((float)loc.getLatitude(), (float)loc.getAltitude());
             temp.setText(String.format("%.1f°C", weather.main.temp));
             minmaxtemp.setText(String.format("%.1f°C/%.1f°C", weather.main.temp_min, weather.main.temp_max));
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
