0ef12def51cd9017fc9df183f8fa5fdb7f6f5beb
Hyunbin Lee <46880076+jshyunbin@users.noreply.github.com>
2019-11-06 09:18:11 +0900

    GPS bug fixed

--- app/src/main/java/com/example/android_api_test/MainActivity.java
+++ app/src/main/java/com/example/android_api_test/MainActivity.java
@@ -26,38 +26,7 @@ import java.util.Calendar;
 public class MainActivity extends Activity {
 
     LocationManager locationManager;
-    LocationListener locationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(Location location) {
-            double lat = location.getLatitude();
-            double lon = location.getLongitude();
-            try {
-                CurrentWeatherData weather = OpenWeatherAPI.getCurrentWeatherData((float)lat, (float)lon);
-                float t = weather.main.temp;
-                float mint = weather.main.temp_min;
-                float maxt = weather.main.temp_max;
-                temp.setText(String.format("%.1f°C", t));
-                minmaxtemp.setText(String.format("%.1f°C/%.1f°C", mint, maxt));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-
-        }
-    };
+    LocationListener locationListener;
 
     TextView time, date, temp, minmaxtemp;
     ImageView weather_icon;
@@ -81,25 +50,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-//        assert locationManager != null;
-//        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-//            new AlertDialog.Builder(this)
-//                    .setTitle("GPS Disabled")
-//                    .setMessage("Please Turn on your GPS to get weather information of your location.")
-//                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-//                        @Override
-//                        public void onClick(DialogInterface dialog, int which) {
-//                            finish();
-//                            System.exit(0);
-//                        }
-//                    })
-//                    .setIcon(android.R.drawable.ic_dialog_alert)
-//                    .show();
-//        }
-//
-//        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 10, locationListener);
-
         time = findViewById(R.id.current_time);
         date = findViewById(R.id.current_date);
         temp = findViewById(R.id.temp);
@@ -109,6 +59,38 @@ public class MainActivity extends Activity {
 
         updateTime.sendEmptyMessage(1);
 
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                double lat = location.getLatitude();
+                double lon = location.getLongitude();
+                try {
+                    CurrentWeatherData weather = OpenWeatherAPI.getCurrentWeatherData((float)lat, (float)lon);
+                    float t = weather.main.temp;
+                    float mint = weather.main.temp_min;
+                    float maxt = weather.main.temp_max;
+                    temp.setText(String.format("%.1f°C", t));
+                    minmaxtemp.setText(String.format("%.1f°C/%.1f°C", mint, maxt));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
         checkLocationPermission();
     }
 
